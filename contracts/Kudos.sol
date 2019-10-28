9eb46bd509ca072a59858ea27bc43a23ffe63025
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-28 11:10:41 +0000

    draft kudos smart contract

---
+++ contracts/Kudos.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.10;
+
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Mintable.sol";
+
+/**
+ * @title KudosCore
+ * @dev Kudos control contract
+ */
+contract KudosCore is Initializable, ERC721, ERC721Mintable {
+
+    /**
+     * @dev Initializer method coded acording OZ system
+     * @param _owner address of the contract owner
+     */
+    function initializeMeetup(address _owner) public initializer {
+        ERC721.initialize();
+        ERC721Mintable.initialize(_owner);
+    }
+}
