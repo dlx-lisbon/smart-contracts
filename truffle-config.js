a26d2a523be8036361a619649df8f9689437a445
Bernardo Vieira <bernardo5@live.com.pt>
2019-11-22 08:56:29 +0000

    update dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -12,10 +12,6 @@
  * to sign your transactions before they're sent to a remote public node. Infura API
  * keys are available for free at: infura.io/register
  *
- *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
- *
- *   > > $ npm install truffle-hdwallet-provider@web3-one
- *
  * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
  * public/private key pairs. If you're publishing your code to GitHub make sure you load this
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
@@ -55,13 +51,11 @@ module.exports = {
 
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        ropsten: {
-            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
-            network_id: 3, // Ropsten's id
-            gas: 5500000, // Ropsten has a lower block limit than mainnet
+        goerli: {
+            provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+            network_id: 5, // Ropsten's id
             confirmations: 2, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-            skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
         },
     },
 
@@ -80,7 +74,7 @@ module.exports = {
                     enabled: false,
                     runs: 200,
                 },
-                evmVersion: 'byzantium',
+                evmVersion: 'constantinople',
             },
         },
     },
