0617bd9deb7d97478c6ef94cbee7ccd1a66b1198
Bernardo Vieira <bernardo5@live.com.pt>
2020-01-13 08:25:47 +0000

    rename init method in kudoscore

--- contracts/KudosCore.sol
+++ contracts/KudosCore.sol
@@ -15,7 +15,7 @@ contract KudosCore is Initializable, ERC721, ERC721Mintable {
      * @dev Initializer method coded acording OZ system
      * @param _owner address of the contract owner
      */
-    function initializeMeetup(address _owner) public initializer {
+    function initializeKudos(address _owner) public initializer {
         ERC721.initialize();
         ERC721Mintable.initialize(_owner);
     }
