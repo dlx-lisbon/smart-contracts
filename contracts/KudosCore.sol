5ef2190a9d862b63c605144bb14fba6d2bcf8bd6
Bernardo Vieira <bernardo5@live.com.pt>
2020-01-27 10:27:58 +0000

    refactor kudos core to erc721full

--- contracts/KudosCore.sol
+++ contracts/KudosCore.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721MetadataMintable.sol";
 
 
@@ -9,12 +8,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721MetadataMintable.sol";
  * @title KudosCore
  * @dev Kudos control contract
  */
-contract KudosCore is ERC721, ERC721Mintable, ERC721MetadataMintable {
+contract KudosCore is ERC721Full, ERC721MetadataMintable {
 
     /**
      * @dev Constructor method initializing KudosCore
      */
-    constructor() public ERC721() {
+    constructor()
+        public
+        ERC721Full("KudosDLX", "KDLX")
+    {
         //
     }
 }
